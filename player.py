aaa19004ad599db3e82212dbed2b47e7c809aaeb
Catherine David <miscatulated@gmail.com>
2022-07-05 14:00:22 -0700

    refactoring for clarity

--- player.py
+++ player.py
@@ -18,6 +18,104 @@ parser.add_argument('--model', type=str, default='', help='model file to load')
 parser.add_argument('--device', type=str, default='cuda', help='device to use')
 args = parser.parse_args()
 
+class PongEnv():
+    def __init__(self):
+        self.paddle_width = 100
+        self.paddle_height = 5
+        self.paddle_speed = 5
+
+        self.ball_loc = None
+        self.ball_speed = None
+        self.paddle_loc = None
+
+        pygame.init()
+
+        self.HEIGHT = 800
+        self.WIDTH = 500
+
+        self.screen = pygame.display.set_mode([self.WIDTH, self.HEIGHT])
+
+        self.GAME_LIMIT = 1000
+
+    def reset(self):
+        self.ball_loc = [250, 0]
+        self.ball_speed = [5, 5]
+        self.paddle_loc = [250, self.HEIGHT-self.paddle_height-5]
+        self.game_counter = 0
+        self.old_pixels = None
+
+    def step(self, action, render=False):
+        reward = 0
+        done = False
+        self.game_counter += 1
+        if self.game_counter > self.GAME_LIMIT:
+            done = True
+
+        # 0: still, 1: left, 2: right
+        if action == 0:
+            pass
+        elif action == 1:
+            self.paddle_loc[0] = self.paddle_loc[0] - self.paddle_speed
+            if self.paddle_loc[0] < 0:
+                self.paddle_loc[0] = 0
+        elif action == 2:
+            self.paddle_loc[0] = self.paddle_loc[0] + self.paddle_speed
+            if self.paddle_loc[0] > self.WIDTH:
+                self.paddle_loc[0] = self.WIDTH
+            
+
+        # Advance the ball
+        if self.ball_loc[1] < 0:
+            self.ball_speed[1] = -self.ball_speed[1]
+        
+        self.ball_loc[0] += self.ball_speed[0]
+        if self.ball_loc[0] > self.WIDTH:
+            self.ball_speed[0] = -self.ball_speed[0]
+        if self.ball_loc[0] < 0:
+            self.ball_speed[0] = -self.ball_speed[0]
+        
+        self.ball_loc[1] += self.ball_speed[1]
+
+        if self.ball_loc[1] > self.HEIGHT-self.paddle_height-5:
+            if (self.ball_loc[0] > self.paddle_loc[0] - self.paddle_width/2) and \
+               (self.ball_loc[0] < self.paddle_loc[0] + self.paddle_width/2):
+                self.ball_speed[1] = -self.ball_speed[1]
+                reward = 100.0
+            else:
+                # Reset the ball
+                self.ball_loc[1] = 0
+                reward = -100.0
+
+
+        # Render image to get next state
+        self.screen.fill((255, 255, 255))
+
+        # Draw a solid blue circle in the center
+        pygame.draw.circle(self.screen, (0, 0, 0), self.ball_loc, 5)
+        pygame.draw.rect(self.screen, (0, 0, 0), (self.paddle_loc[0]-self.paddle_width/2, self.paddle_loc[1], self.paddle_width, self.paddle_height))
+        
+        if render:
+            # Flip the display
+            pygame.display.flip()
+            # sleep for a bit
+            pygame.time.delay(10)
+
+        # can we just reference them?
+        small = pygame.transform.scale(self.screen, (128, 128))
+
+        pixels = torch.tensor(1 - (pygame.surfarray.array_red(small) / 255.0)).float().unsqueeze(0)
+        if self.old_pixels is None:
+            self.old_pixels = pixels
+
+        pixels_diff = pixels - self.old_pixels
+
+        self.old_pixels = pixels
+        
+        # return values are (state, reward, done)
+        return pixels, reward, done
+        
+
+
 class GameModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
@@ -77,33 +175,14 @@ running_average_loss = 0.0
 reward = 0.0
 
 hits = 0.0
+rendering = False
 
 if __name__=="__main__":
+    env = PongEnv()
+    env.reset()
     # game stuff
-
-    pygame.init()
-
-    HEIGHT = 800
-    WIDTH = 500
-
-    screen = pygame.display.set_mode([WIDTH, HEIGHT])
-
-    ball_loc = [250, 0]
-    ball_speed = [5, 5]
-
-    paddle_width = 100
-    paddle_height = 5
-    paddle_loc = [250, HEIGHT-paddle_height-5]
-    paddle_speed = 10
-
-    moving_left = False
-    moving_right = False
-    ml_control = True # always true because rl
-
     running = True
     agent_score = 0
-
-    rendering = True
 
     counter = 0
 
@@ -114,34 +193,13 @@ if __name__=="__main__":
     random_action = 0
     random_movement_counter = 0
 
+    pixels_diff, reward, done = env.step(random_action)
+
     while running:
         counter += 1
         #print(foo_counter)
         # Fill the background with white
-        screen.fill((255, 255, 255))
-
-        # Draw a solid blue circle in the center
-        pygame.draw.circle(screen, (0, 0, 0), ball_loc, 5)
-        pygame.draw.rect(screen, (0, 0, 0), (paddle_loc[0]-paddle_width/2, paddle_loc[1], paddle_width, paddle_height))
         
-        if rendering:
-            # Flip the display
-            pygame.display.flip()
-            # sleep for a bit
-            pygame.time.delay(10)
-
-        # can we just reference them?
-        small = pygame.transform.scale(screen, (128, 128))
-
-        pixels = torch.tensor(1 - (pygame.surfarray.array_red(small) / 255.0)).float().unsqueeze(0)
-        if old_pixels is None:
-            old_pixels = pixels
-
-        pixels_diff = pixels - old_pixels
-
-        old_pixels = pixels
-        
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -150,83 +208,24 @@ if __name__=="__main__":
                     rendering = not rendering
 
 
+
         # predict value of each action
         action_values = model(pixels_diff.unsqueeze(0).to(device))[0]
 
-        ai_moving_left = False
-        ai_moving_right = False
+        if random_movement_counter == 0 and random.random() < 0.01:
+            random_movement_counter = 20
+            random_action = random.randint(0, 2)
 
-        if ml_control:
-            #print(random_movement_counter, random_action)
-            # overwrite player action
-            if random_movement_counter == 0 and random.random() < 0.01:
-                random_movement_counter = 20
-                random_action = random.randint(0, 2)
-                #print("choosing randomly")
-            else:
-                #print(action_values)
-                # RSI select probabilistically
-                #action_probs = torch.nn.functional.softmax(torch.tensor(action_values), dim=0)
-                #action_to_pick = torch.multinomial(action_probs, 1)[0].item()
-                action_to_pick = torch.argmax(action_values).item()
-                #print("picking", action_values, action_to_pick)
-
-            if random_movement_counter > 0:
-                random_movement_counter -= 1
-                action_to_pick = random_action
-                
-            #print("Picked ", action_to_pick)
-            #print
-
-            if action_to_pick == 0:
-                pass
-            elif action_to_pick == 1:
-                ai_moving_left = True
-                ai_moving_right = False
-            elif action_to_pick == 2:
-                ai_moving_left = False
-                ai_moving_right = True
         else:
-            print("unsupported")
-            pass
+            action_to_pick = torch.argmax(action_values).item()
 
-        new_reward = 0.0
-
-        # update physics for next frame
-        if moving_left or ai_moving_left:
-            paddle_loc[0] -= 5
-            if paddle_loc[0] < 0:
-                paddle_loc[0] = 0
-        if moving_right or ai_moving_right:
-            paddle_loc[0] += 5
-            if paddle_loc[0] > WIDTH:
-                paddle_loc[0] = WIDTH
-
-        ball_loc[0] += ball_speed[0]
-        ball_loc[1] += ball_speed[1]
-
-        #torch.autograd.set_detect_anomaly(True)
-
-        if ball_loc[1] > HEIGHT-paddle_height-5:
-            if ball_loc[0] > paddle_loc[0] - paddle_width/2 and ball_loc[0] < paddle_loc[0] + paddle_width/2:
-                ball_speed[1] = -ball_speed[1]
-                new_reward = 100.0
-                hits += 1
-            else:
-                # Reset, episode over
-                ball_loc[1] = 0
-                new_reward = -100.0
+        if random_movement_counter > 0:
+            random_movement_counter -= 1
+            action_to_pick = random_action
 
 
-        # Advance the ball
-        if ball_loc[1] < 0:
-            ball_speed[1] = -ball_speed[1]
-        if ball_loc[0] > WIDTH:
-            ball_speed[0] = -ball_speed[0]
-        if ball_loc[0] < 0:
-            ball_speed[0] = -ball_speed[0]
- 
-
+        pixels_diff, new_reward, done = env.step(action_to_pick, render=rendering)
+        
         # save state and reward
         #print(action_states)
         if last_state_and_action is not None:
