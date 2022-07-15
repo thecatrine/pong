fcf6d41eeac5b766bbbfbb733b3d9e1b15f6f8c5
Catherine David <miscatulated@gmail.com>
2022-07-15 00:43:42 -0700

    saving

--- player.py
+++ player.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 
 import concurrent.futures
 from torch.utils.tensorboard import SummaryWriter
+from torch import autograd
 writer = SummaryWriter()
 #model stuff
 
@@ -144,6 +145,8 @@ class PongEnv():
         self.old_game_state = game_state
         
         # return values are (state, reward, done)
+        #if reward != 0:
+        #    import pdb; pdb.set_trace()
         return pixels_to_return, reward, done
         
 
@@ -199,34 +202,21 @@ def compute_loss(net, obvs, actions, rewards):
     if logprobs.shape != rewards.shape:
         import pdb; pdb.set_trace()
 
-    #import pdb; pdb.set_trace()
-
     return -torch.mean(logprobs * rewards)
 
-EPISODE_LENGTH = 200
-
-def collect_one_episode(r_net, env, render):
-    memory = []
-    state, reward, done = env.step()
-
-    for step in range(EPISODE_LENGTH):
-        first_obvs = state.to(device)
-        x = r_net(first_obvs.unsqueeze(0))
-
-        act_probs = r_net.policy(x)
-        act = torch.multinomial(act_probs, 1).item()
-
-        state, reward, done = env.step(act, render=render)
-
-        second_obvs = state.to(device)
-        memory.append((first_obvs, second_obvs, torch.tensor([act]), torch.tensor([reward])))
+EPISODE_LENGTH = 33
 
 
-    return memory
+def gradient_norm(model):
+    parameters = [p for p in model.parameters() if p.grad is not None]
+    total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach(), 2).to(device) for p in parameters]), 2)
+    return total_norm
 
 # globals
 rendering = True
 running = True
+
+NUM_THREADS = 32
 
 def train_one_epoch(r_net, r_optimizer, envs):
     global rendering
@@ -236,7 +226,6 @@ def train_one_epoch(r_net, r_optimizer, envs):
 
 
     BATCH_SIZE = 1000
-    BUFFER_SIZE = 200
 
     ALPHA = 0.95
 
@@ -246,93 +235,154 @@ def train_one_epoch(r_net, r_optimizer, envs):
     saved_r_model = RewardNetwork().to(device)
     saved_r_model.load_state_dict(r_net.state_dict())
 
-    NUM_THREADS = 10
-
     batch_lens = []
 
     reporting_actor_loss = []
     reporting_score_loss = []
     reporting_entropy_loss = []
     reporting_rewards = []
+
+    all_first_obvs =  [[] for _ in range(NUM_THREADS)]
+    all_after_obvs =  [[] for _ in range(NUM_THREADS)]
+    all_actions =  [[] for _ in range(NUM_THREADS)]
+    all_after_actions = [[] for _ in range(NUM_THREADS)]
+    all_rewards =  [[] for _ in range(NUM_THREADS)]
+
+    state = [None for _ in range(NUM_THREADS)]
     
-    for batch in range(NUM_THREADS):
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [executor.submit(collect_one_episode, r_net, envs[i], False) for i in range(len(envs)-1)]
+    for env_i in range(NUM_THREADS):
+        state[env_i] = envs[env_i].step()[0]
+
+    for step in range(EPISODE_LENGTH):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    rendering = not rendering
+       
+        first_obvs = torch.stack(state).to(device)
+        x = r_net(first_obvs)
+        act_probs = r_net.policy(x)
         
-        shown_result = collect_one_episode(r_net, envs[-1], True)
+        # take a random action 3% of the time
+        act = torch.multinomial(act_probs, 1)
 
-        results = shown_result
-        for f in futures:
-            results += f.result()
+       
+        if rendering:
+            print(
+                "probs: ", act_probs[0].cpu().detach().numpy(),
+                "action: ", act[0].cpu().detach().numpy(),
+                "pred scores: ", r_net.reward(x)[0].cpu().detach().numpy(),
+            )
+
+        for env_i in range(NUM_THREADS):
+            new_state, r, d = envs[env_i].step(act[env_i], render=(rendering and env_i == 0))
+            
+            all_first_obvs[env_i].append(state[env_i].to(device))
+            all_after_obvs[env_i].append(new_state.to(device))
+            all_actions[env_i].append(act[env_i].item())
+            
+            if step != 0:
+                all_after_actions[env_i].append(act[env_i].item())
+
+            #if r != 0:
+            #    import pdb; pdb.set_trace()
+            all_rewards[env_i].append(r)
+            state[env_i] = new_state
+
+    # do one last step for the last state
+    first_obvs = torch.stack(state)
+    x = r_net(first_obvs.to(device))
+    act_probs = r_net.policy(x)
+    act = torch.multinomial(act_probs, 1)
+    for env_i in range(NUM_THREADS):
+        all_after_actions[env_i].append(act[env_i].item())
         
-        batch_lens.append(EPISODE_LENGTH * NUM_THREADS)
+    # Now do training
 
-        first_obvs, after_obvs, actions, rewards = map(torch.stack, zip(*results))
-        actions = actions.to(device)
+    batch_lens.append(EPISODE_LENGTH * NUM_THREADS)
 
-        # normalize rewards
-        #rewards = torch.nn.functional.normalize(rewards, dim=-1)
-            
-        r_optimizer.zero_grad()
+    first_obvs = torch.cat([torch.stack(x) for x in all_first_obvs], dim=0)
+    after_obvs = torch.cat([torch.stack(x) for x in all_after_obvs], dim=0)
+    actions = torch.cat([torch.tensor(x).to(device) for x in all_actions], dim=0).unsqueeze(-1)
+    after_actions = torch.cat([torch.tensor(x).to(device) for x in all_after_actions], dim=0).unsqueeze(-1)
+    after_obvs_batches = torch.stack([torch.stack(x) for x in all_after_obvs])
+    last_step = after_obvs_batches[:, -1, :]
+    last_x = r_net(last_step)
+    last_guess = r_net.reward(last_x)
+    last_action_probs = r_net.policy(last_x)
+    last_action = torch.multinomial(last_action_probs, 1)
+    last_value = torch.gather(last_guess, dim=1, index = last_action)
+    
+    orig_rewards = torch.stack([torch.tensor(x).to(device) for x in all_rewards])
+    rewards = orig_rewards.clone()
 
-        predicted_scores = r_net.reward(r_net(first_obvs))
+    rewards[:, -1] += last_value[:, 0]
+    for step in range(EPISODE_LENGTH-2, -1, -1):
+        rewards[:, step] = rewards[:, step] + ALPHA * rewards[:, step+1]
 
-        predicted_and_chosen_scores = predicted_scores.gather(1, actions)
+    
+    rewards = rewards.view(-1, 1)
 
-        # Can we reuse the one we already have? RSI
-        x = r_net(after_obvs)
-        chosen_acts = torch.multinomial(r_net.policy(x), 1)
+    # have to do rollout reward logic specially
+    r_optimizer.zero_grad()
+    #with autograd.detect_anomaly():
+    predicted_scores = r_net.reward(r_net(first_obvs))
 
-        old_preds = saved_r_model.reward(
-            saved_r_model(after_obvs)
-        ).gather(
-            1,
-            chosen_acts.to(device),
-        )
+    #import pdb; pdb.set_trace()
+    predicted_and_chosen_scores = predicted_scores.gather(1, actions)
 
-        final_guess = old_preds[-1][0]
-        #import pdb; pdb.set_trace()
+    old_preds = saved_r_model.reward(
+        saved_r_model(after_obvs.to(device))
+    ).gather(
+        1,
+        after_actions.to(device),
+    )
 
-        actual_scores = rewards.to(device).clone()
-        actual_scores[-1] += final_guess
-        for jj in range(len(actual_scores) - 2, -1, -1):
-            actual_scores[jj] += actual_scores[jj + 1] * ALPHA
 
-        loss_fn = torch.nn.SmoothL1Loss()
-        score_loss = loss_fn(predicted_and_chosen_scores, actual_scores)
+    actual_scores = rewards
 
-        # Actor
+    loss_fn = torch.nn.MSELoss()
+    score_loss = loss_fn(predicted_and_chosen_scores, actual_scores)
 
-        advantage = actual_scores - predicted_and_chosen_scores.detach()
+    # Actor
 
-        #import pdb; pdb.set_trace()
-        actor_loss = compute_loss(
-            r_net,
-            first_obvs, 
-            actions, 
-            advantage.to(device),
-        )
+    advantage = actual_scores - predicted_and_chosen_scores.detach()
 
-        # Entropy Loss
+    #import pdb; pdb.set_trace()
+    actor_loss = compute_loss(
+        r_net,
+        first_obvs, 
+        actions, 
+        advantage.to(device),
+    )
 
-        x = r_net(first_obvs.to(device))
-        act_probs2 = r_net.policy(x)
-        act_probs2 = act_probs2.gather(1, actions)
+    # Entropy Loss
 
-        entropy_loss = torch.sum(act_probs2 * torch.log(act_probs2))
-            
-        total_loss = 1*score_loss + 0.5*actor_loss + 0.003*entropy_loss
-        total_loss.backward()
+    act_probs2 = r_net.policy(r_net(first_obvs))
+    act_probs2 = act_probs2.gather(1, actions)
 
-        r_optimizer.step()
+    entropy_loss = torch.mean(act_probs2 * torch.log(act_probs2))
+        
+    total_loss = 1*score_loss + 0.5*actor_loss + 2*entropy_loss
 
-        reporting_actor_loss.append(actor_loss.item())
-        reporting_score_loss.append(score_loss.item())
-        reporting_entropy_loss.append(entropy_loss.item())
-        reporting_rewards.append(rewards.sum())
+    total_loss.backward()
+    # end detect anomaly
 
-    # save changes to value net
-    saved_r_model.load_state_dict(r_net.state_dict())
+    torch.nn.utils.clip_grad_norm_(r_net.parameters(), 200)
+
+    r_optimizer.step()
+
+    reporting_actor_loss.append(actor_loss.item())
+    reporting_score_loss.append(score_loss.item())
+    reporting_entropy_loss.append(entropy_loss.item())
+    reporting_rewards.append(orig_rewards.sum())
+
+    #import pdb; pdb.set_trace()
+
+    #import pdb; pdb.set_trace()
 
     return torch.mean(torch.tensor(reporting_actor_loss), dtype=torch.float), \
         torch.mean(torch.tensor(reporting_score_loss), dtype=torch.float),    \
@@ -348,7 +398,7 @@ reward_model = RewardNetwork()
 
 #loss_fn = torch.nn.CrossEntropyLoss()
 #loss_fn = torch.nn.SmoothL1Loss()
-reward_optimizer = torch.optim.AdamW(reward_model.parameters(), lr=1e-3)
+reward_optimizer = torch.optim.AdamW(reward_model.parameters(), lr=3e-4)
 
 # initialize device
 device = torch.device(args.device)
@@ -356,9 +406,10 @@ reward_model.to(device)
 
 
 if __name__=="__main__":
-    envs = [PongEnv() for i in range(10)]
-    for env in envs:
-        env.reset()
+    envs = [PongEnv() for i in range(NUM_THREADS)]
+    for i in range(NUM_THREADS):
+        envs[i].reset()
+        [envs[i].step() for j in range(i*EPISODE_LENGTH)]
 
     i = 0
     while running:
